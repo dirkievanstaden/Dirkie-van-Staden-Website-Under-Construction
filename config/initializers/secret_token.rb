407ab8a4980e6afb2f493ebf98a72acecd07945c
Dirkie van Staden <dirkievanstaden@gmail.com>
2012-01-13 16:10:26 +0200

    commit1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blou::Application.config.secret_token = '3ad11d243338562939e08ae9a81ac6e2c9b1e68010fa227adb6365fbf132221dbc5f1c0e6f53b20ff294d64bacfab24277c9310b73c0120984abea4827ea8fe9'
